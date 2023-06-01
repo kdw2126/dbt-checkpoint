e62e1433f15b36da82d4dd73b3bb8aece2c04d81
Kyle D. Weber <54750041+kdw2126@users.noreply.github.com>
2023-06-01 12:22:51 -0400

    Update dbt_compile.py

--- dbt_checkpoint/dbt_compile.py
+++ dbt_checkpoint/dbt_compile.py
@@ -27,8 +27,9 @@ def prepare_cmd(
         dbt_models = models
     else:
         dbt_models = paths_to_dbt_models(paths, prefix, postfix)
+    print(dbt_models)
+    print(cmd_flags)    
     cmd = ["dbt", *global_flags, "compile", "-m", *dbt_models, *cmd_flags]
-    print(cmd)
     return cmd
 
 
